d758e3006024ae8c136bb4791b92d6d8eb675aff
Alexis Manin <alexis.manin@gmail.com>
2022-08-20 15:30:41 +0200

    refactor(Build): introduce a version catalog.
    Also: update Kotlin to 1.7.10, Spring-Boot to 2.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,9 @@
 group = "fr.amanin"
 version = "0.1"
 
-val ktorVersion = "1.3.1"
-val exposedVersion = "0.24.1"
-
 plugins {
     java
-    val ktVersion = "1.5.0"
+    val ktVersion = "1.7.10"
     kotlin("jvm") version ktVersion
     kotlin("plugin.serialization") version ktVersion
 }
@@ -16,43 +13,47 @@ repositories {
     mavenLocal()
     mavenCentral()
     jcenter()
-    maven(url="http://repo.spring.io/release")
+    maven(url="https://repo.spring.io/release")
 }
 
+val springBootVersion = "2.7.3"
+
 dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.3"))
+
     // Kt specifics
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+
+    implementation(kotlinx.coroutines)
     // Kt serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation(kotlinx.serialization.json)
 
     // Spring reactive stack
-    implementation("org.springframework:spring-webflux:5.3.7")
-    implementation(platform("io.projectreactor:reactor-bom:2020.0.7"))
+    implementation("org.springframework:spring-webflux")
+//    implementation(platform("io.projectreactor:reactor-bom:2020.0.7"))
 //    implementation("io.projectreactor:reactor-core")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("io.projectreactor.netty:reactor-netty")
 
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
     // For SO answer: https://stackoverflow.com/questions/61312690/how-to-elegantly-serialize-and-deserialize-opencv-yaml-calibration-data-in-java/
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.10.3")
-    implementation("org.openpnp:opencv:4.3.0-1")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation(libs.opencv)
 
     // For fr.amanin.stackoverflow.ktorstaticaccess
-    implementation("io.ktor:ktor-server-jetty:$ktorVersion")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.5")
+    implementation(libs.ktor.server.jetty)
+    implementation("com.github.ben-manes.caffeine:caffeine")
 
     // For SO question https://stackoverflow.com/questions/64137800/kotlin-and-exposed-orm
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
-    implementation("com.h2database:h2:1.4.199")
-    implementation("com.zaxxer", "HikariCP", "3.4.5")
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation("com.h2database:h2")
+    implementation("com.zaxxer:HikariCP")
 }
 
 configure<JavaPluginConvention> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,28 @@
 rootProject.name = "reactive-sandbox"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("kotlinx") {
+            library("serialization.json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+            library("coroutines", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+        }
+
+        create("libs") {
+            // library("exposed", )
+            library("opencv", "org.openpnp:opencv:4.3.0-1")
+
+            version("exposed", "0.24.1").let {
+                val grp = "org.jetbrains.exposed"
+                library("exposed.core", grp, "exposed-core").versionRef(it)
+                library("exposed.dao", grp, "exposed-dao").versionRef(it)
+                library("exposed.jdbc", grp, "exposed-jdbc").versionRef(it)
+                library("exposed.java.time", grp, "exposed-java-time").versionRef(it)
+            }
+
+            version("ktor", "1.3.1").let {
+                val grp = "io.ktor"
+                library("ktor.server.jetty", grp, "ktor-server-jetty").versionRef(it)
+            }
+        }
+    }
+}
